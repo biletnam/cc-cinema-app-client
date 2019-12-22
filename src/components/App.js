9e7b1c26d891f4888c43d16758689beb9214853b
Jakub Figlak <jakub.figlak@gmail.com>
2019-12-22 16:42:01 +0100

    Create navbar and add some basic styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MainPage from './LandingPage/MainPage';
 import MoviesList from './Movies/MoviesList';
 import ShowsList from './Shows/ShowsList';
 import Reservation from './Reservation/Reservation';
+import Header from './Header';
+
+import '../styles/main.scss';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={MainPage} />
           <Route path="/movies" exact component={MoviesList} />
           <Route path="/shows" exact component={ShowsList} />
